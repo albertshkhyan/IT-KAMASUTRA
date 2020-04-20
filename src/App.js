1e492c8da0e153c32cf5ac9ef56ada818445eb8e
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-04-21 00:30:12 +0400

    Refactoring - CreateMyContext.Proveider -> Provider

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 function App(props) {
+  debugger
+
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Dashboard />
         <div className="app-wrapper-content">
-          <Route exact path="/dialogs" render={() => ( <DialogsContainer  />  )} />
-          <Route path="/profile" render={() => (  <Profile /> )} />
+          <Route exact path="/dialogs" component={DialogsContainer} />
+          <Route path="/profile" component={Profile} />
         </div>
       </div>
     </BrowserRouter>
--- src/createMyContext.js
+++ src/createMyContext.js
@@ -2,5 +2,12 @@ import React from 'react'
 
 const CreateMyContext = React.createContext(null);
 
+export const Provider = props => {
+    return (
+        <CreateMyContext.Provider value={props.store}>
+            {props.children}
+        </CreateMyContext.Provider>
+    )
+}
 
 export default CreateMyContext;
