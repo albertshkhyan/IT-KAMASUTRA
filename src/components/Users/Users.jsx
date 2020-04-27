9b322516820ac704dd77169c307e472771cbcb27
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-04-27 22:17:24 +0400

    add if in componentDidMount. For switched not doubled users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,52 +2,57 @@ import React from "react";
 
 import * as axios from "axios";
 
-import userPhoto from '../../assets/images/users.png';
+import userPhoto from "../../assets/images/users.png";
 
 import classes from "./style.module.css";
 
-
 class Users extends React.Component {
   componentDidMount() {
-    axios
-    .get("https://social-network.samuraijs.com/api/1.0/users")
-    .then( (response) =>  this.props.setUsers(response.data.items) );
+    if (this.props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => this.props.setUsers(response.data.items));
+    }
   }
-
-  render(){
+  render() {
     return (
       <div className="Users">
-      {this.props.users.map((item) => {
-        return (
-          <div key={item.id} className={classes.userContaienr}>
-            <div className={classes.avatar}>
-              <img src={item.photos.small ? item.photos.small  : userPhoto} alt={item.description} />
-            </div>
+        {this.props.users.map((item) => {
+          return (
+            <div key={item.id} className={classes.userContaienr}>
+              <div className={classes.avatar}>
+                <img
+                  src={item.photos.small ? item.photos.small : userPhoto}
+                  alt={item.description}
+                />
+              </div>
               <div className="name">
                 <span>{item.name}</span>
               </div>
 
-            <div className={classes.message}>
-              <span>{item.description}</span>
-            </div>
+              <div className={classes.message}>
+                <span>{item.description}</span>
+              </div>
 
-            <div className={classes.btn}>
-              {item.followed ? (
-                <button onClick={() => this.props.follow(item.id)}>FOLLOW</button>
-              ) : (
-                <button onClick={() => this.props.unfollow(item.id)}>
-                  UNFOLLOW
-                </button>
-              )}
+              <div className={classes.btn}>
+                {item.followed ? (
+                  <button onClick={() => this.props.follow(item.id)}>
+                    FOLLOW
+                  </button>
+                ) : (
+                  <button onClick={() => this.props.unfollow(item.id)}>
+                    UNFOLLOW
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <div className={`${classes.btnShowMoreContaoner}  flexible jCenter`}>
-        <button onClick={this.getUsers}>SHOW MORE</button>
+          );
+        })}
+        <div className={`${classes.btnShowMoreContaoner}  flexible jCenter`}>
+          <button onClick={this.getUsers}>SHOW MORE</button>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
