0556654a44e6cd29642d225d5f7539b3cdda9f52
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-05-07 21:34:58 +0400

    add HeaderContainer, add authReducer, cookie, login, auth/me

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from 'redux';
 import dialogPageReducer from './dialogPageReducer';
 import profilePageReducer from './profilePageReducer';
 import userPageReducer from './userPageReducer';
+import authReducer from './authReducer';
 
 const reducers = combineReducers({
     dialogPage: dialogPageReducer,
     profilePage: profilePageReducer,
-    usersPage: userPageReducer
+    usersPage: userPageReducer,
+    auth : authReducer
 });
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
