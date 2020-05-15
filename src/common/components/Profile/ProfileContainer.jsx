4d9cd9546e22bf4afbc4e9de30b4efcca7a23faa
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-05-15 14:53:02 +0400

    create container compoennt for Redirect, and that component wrapped connect for isAuth, FOR SAVE SNGLE RESPONSIBILITY PRINCIP, not created HOC, still :(

--- src/common/components/Profile/ProfileContainer.jsx
+++ src/common/components/Profile/ProfileContainer.jsx
@@ -8,23 +8,19 @@ import Profile from "./Profile";
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
     let { userID } = this.props.match.params;
     this.props.profileThunkCreator(userID);
-
-    // APIRequests.profile(userID).then((data) => {
-    //   this.props.setProfileData(data);
-    // });
   }
 
-  render() {  
-    //// NOTE - No good
-    if (!this.props.isAuth) {
-      return <Redirect to="/login" />;
-    }    
+  render() {
+    //// NOTE - No good -> here we are breaking Single Responsibility, Redirect must be wrapped another contianer component
+    // if (!this.props.isAuth) {
+    //   return <Redirect to="/login" />;
+    // }
     return <Profile {...this.props} />;
   }
 }
@@ -33,22 +29,24 @@ const mapStateToProps = (state) => {
   return {
     isFetching: state.usersPage.isFetching,
     profileData: state.profilePage.profileData,
-    isAuth: state.auth.isAuth,
   };
 };
 
-////variant 1
-// const GetURLContainer = withRouter(ProfileContainer);
-// export default connect(mapStateToProps, {
-//   setIsFetching,
-//   setUsers,
-//   setProfileData,
-// })(GetURLContainer);
+////NOTE create container compoennt for Redirect, and that component wrapped connect for isAuth, FOR SAVE SNGLE RESPONSIBILITY PRINCIP
+const mapStateToPropsForRedirectContainer = (state) => ({
+    isAuth: state.auth.isAuth,
+});
+const RedirectContainer = (props) => {
+  if (!props.isAuth) {
+    return <Redirect to="/login" />;
+  }
+  return <ProfileContainer {...props}/>;
+};
+const AuthRedirectContainer = connect(mapStateToPropsForRedirectContainer, null)(RedirectContainer);
 
-////variant 2
 export default connect(mapStateToProps, {
   setUsers,
   setIsFetching,
   setProfileData,
   profileThunkCreator,
-})(withRouter(ProfileContainer));
+})(withRouter(AuthRedirectContainer));
