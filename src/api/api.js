8d40964cc96eb62581242a319d6127db5bfd0861
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-05-27 19:01:41 +0400

    Now we can login and logout, create Redirect in Login.jsx, becaouse hoc of Reidrect have  bug

--- src/api/api.js
+++ src/api/api.js
@@ -58,10 +58,16 @@ const authAPI = {
     authMe() {
         return instance.get(`auth/me`);
     },
+    login(email, password, rememberMe = false) {
+        return instance.post(`auth/login`, { email, password, rememberMe });
+    },
+    logout() {
+        return instance.delete(`auth/login`);
+    }
 }
 
 const profileAPI = {
-    
+
     profile(userID) {
         return instance
             .get(`profile/${userID}`);
--- src/common/components/Login/Login.jsx
+++ src/common/components/Login/Login.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import LoginReduxForm from "./LoginForm";
+import { connect } from "react-redux";
+import { loginThunkCreator } from "../../../redux/authReducer";
+import { Redirect } from "react-router-dom";
 
 const Login = (props) => {
-  const submit = (some) => {
-      //pass values from store.getState().form
-      //NOTE - In here we can call ascyn actions - for validation
+  const submit = (values) => {
+    //pass values from store.getState().form
+    //NOTE - In here we can call ascyn actions - for validation
     // console.log("some", some);
+    let { email, password, rememberMe } = values;
+    props.loginThunkCreator(email, password, rememberMe);
   };
+  if (props.isAuth) {
+    return <Redirect to="/profile" />;
+  }
   return (
     <div className="Login">
-        {/* we must pass onSubmit props for work */}
+      {/* we must pass onSubmit props for work */}
       {/* <LoginReduxForm on_submit={submit} /> */}
       <LoginReduxForm onSubmit={submit} />
     </div>
   );
 };
+const mapStateToProps = (state) => ({ isAuth: state.auth.isAuth });
 
-export default Login;
+export default connect(mapStateToProps, { loginThunkCreator })(Login);
