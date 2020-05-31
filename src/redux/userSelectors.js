171507063e77b7eb13b055446836d5258a30a66c
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-06-01 03:58:56 +0400

    used reselct lib, getUsers selctor created with createSelector function from reselct lib, :)

--- src/redux/userSelectors.js
+++ src/redux/userSelectors.js
@@ -1,3 +1,6 @@
+import { createSelector } from "reselect";
+
+
 /**
  * A selector is a function that accepts Redux state as an argument and returns data that is derived from that state. Selectors can provide performance optimizations to your application and can also help you encapsulate your global state tree.
  */
@@ -16,11 +19,22 @@ export const getProfileData = state => state.profile.profileData;
 //UserContainer
 
 
+////primitive selector - variant 1
 // export const getUsers = state => state.users.users;//not do re-render, becaiuse this selector return primitve value, BUT -> only called mpaStateToProps 👍
 
-export const getUsers = state => state.users.users.filter(u => true);//mpaStateToProps -> render, mpaStateToProps -> render and etc ♾ 😨😨
+////reference selector- variant 2
+// export const getUsers = state => state.users.users.filter(u => true);//mpaStateToProps -> render, mpaStateToProps -> render and etc ♾ 😨😨
 
 export const getTotalCount = state => state.users.totalCount;
 export const getCurrentPage = state => state.users.currentPage;
 export const getPageSize = state => state.users.pageSize;
 export const getFollowingInProgress = state => state.users.followingInProgress;
+
+
+////with reselect - good perfomance 👍
+
+const getUsersSelector = state => state.users.users;
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+    return users.filter(u => true);
+});
